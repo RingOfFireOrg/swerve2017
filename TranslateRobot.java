3d94040b7188e67e6d5d988069b1e37867f63d37
abigail <abigail@192.168.0.33>
2018-01-02 16:03:11 -0500

    next steps of swerve...

--- TranslateRobot.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team9999.robot;
-
-public class TranslateRobot {
-
-	public TranslateRobot(SwerveModule backRight, SwerveModule backLeft, SwerveModule frontRight, SwerveModule frontLeft) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
