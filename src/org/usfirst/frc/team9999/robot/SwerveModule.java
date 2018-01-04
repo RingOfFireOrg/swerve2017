12db73a8016bf5bc83d97b6ff83ce9f1cc8ed0d6
abigail <abigail@192.168.1.54>
2018-01-04 15:26:33 -0500

    more progress... dont have specific words

--- src/org/usfirst/frc/team9999/robot/SwerveModule.java
+++ src/org/usfirst/frc/team9999/robot/SwerveModule.java
@@ -3,31 +3,45 @@ package org.usfirst.frc.team9999.robot;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.TalonSRX;
 
-public class SwerveModule{
+public class SwerveModule {
 	Jaguar drive;
 	TalonSRX steer;
 	AbsoluteAnalogEncoder turnEncoder;
+	PIDController pid = new PIDController(1, 0, 0, turnEncoder.getAngle, turnSpeed);
 	double speed;
+	double turnSpeed;
 	double angle;
-	
+
 	public SwerveModule(Jaguar driveMotor, TalonSRX steerMotor, AbsoluteAnalogEncoder steerEncoder) {
 		drive = driveMotor;
 		steer = steerMotor;
 		turnEncoder = steerEncoder;
 	}
-	private void setSpeed (double driveSpeed) {
+
+	private void setSpeed(double driveSpeed) {
 		speed = driveSpeed;
 		drive.set(speed);
 	}
+
 	private void setAngle (double wheelAngleGoal) {
 		angle = wheelAngleGoal;
 		double currentAngle = turnEncoder.getAngle();
-		//figure out tolerance level(if absolute value dif is less than something...)
-		//proportional turn
+		steer.set(turnSpeed);
+		pid.setSetpoint(angle);
+		pid.enable
+			while(pid.isenabled == true) {
+				if(math.abs(pid.getSetpoint - turnEncoder.getAngle()) < 5) {
+					pid.disable
+				}
+			}
+		//figure out tolerance level 5 may be wrong number...
+		//also the while loop might not work if it is being called every x ms... maybe do cases with steps for each one
+		
 		
 	}
+
 	public void control(double driveSpeed, double wheelAngle) {
-		setSpeed(driveSpeed);
 		setAngle(wheelAngle);
+		setSpeed(driveSpeed);
 	}
 }
