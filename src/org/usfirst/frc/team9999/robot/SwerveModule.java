64a9111e13b5a8338c2b2789052b9261f2800ee8
Abigail Smith <abigail@randomsmiths.com>
2018-01-23 20:08:12 -0500

    Fixed logic for turning wheels so no while loop

--- src/org/usfirst/frc/team9999/robot/SwerveModule.java
+++ src/org/usfirst/frc/team9999/robot/SwerveModule.java
@@ -10,7 +10,8 @@ public class SwerveModule {
 	PIDController pid = new PIDController(1, 0, 0, turnEncoder.getAngle, turnSpeed);
 	double speed;
 	double turnSpeed;
-	double angle;
+	double angleGoal;
+	double currentAngle;
 
 	public SwerveModule(Jaguar driveMotor, TalonSRX steerMotor, AbsoluteAnalogEncoder steerEncoder) {
 		drive = driveMotor;
@@ -24,16 +25,16 @@ public class SwerveModule {
 	}
 
 	private void setAngle (double wheelAngleGoal) {
-		angle = wheelAngleGoal;
-		double currentAngle = turnEncoder.getAngle();
-		steer.set(turnSpeed);
-		pid.setSetpoint(angle);
-		pid.enable
-			while(pid.isenabled == true) {
-				if(math.abs(pid.getSetpoint - turnEncoder.getAngle()) < 5) {
-					pid.disable
-				}
-			}
+		angleGoal = wheelAngleGoal;
+		currentAngle = turnEncoder.getAngle();
+		if(math.abs(pid.getSetpoint - turnEncoder.getAngle()) < 5) {
+			//nothing
+		} else {
+			pid.setSetpoint(angle);
+			pid.enable
+			steer.set(turnSpeed);
+		}
+		
 		//figure out tolerance level 5 may be wrong number...
 		//also the while loop might not work if it is being called every x ms... maybe do cases with steps for each one
 		
